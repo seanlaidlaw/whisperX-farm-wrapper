fe5dddcecd4ae6f557454eeb5f322d21ac7d54c1
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2023-09-20 16:35:37 +0100

    update whisperX to version 2 and add json to markdown parsing code for transcript and upload output to notion

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 
 using [whisperX](https://github.com/m-bain/whisperX) to run transcriptions.
-Transcription is intiiated by running `transcribe_whisperX.sh`
 
+Pipeline is run by running ./sync_and_transcribe.zsh inside the whisperx mamba
+environment
+
+this will run a bsub job with the GPU to run whisperX on the input audio file,
+using the google drive folder specified in the text file, and using the hugging
+face api key that is put in hf_token.txt
+
+the output result is uploaded to Notion database as a transcript
--- sync_and_transcribe.sh
+++ sync_and_transcribe.sh
@@ -3,13 +3,15 @@
 # fail script on first error
 set -e
 
-gdrive_folder=$(<google_drive_folder.txt)
+ gdrive_folder=$(<google_drive_folder.txt)
+ hf_token=$(<hf_token.txt)
 
 # download google drive folder for transcribing
 gdown --fuzzy https://drive.google.com/drive/folders/"${gdrive_folder}" -O input_audio --folder
 
 # find audio file in google drive folder
 input_track="$(ls input_audio | grep '.m4a' | head -n 1)"
+
 # get from audiofile the creation date
 creation_time=$(ffprobe input_audio/"$input_track" 2>&1 | grep "creation_time" | sed 's/ *creation_time *: *//g' | tail -n 1)
 
@@ -20,11 +22,12 @@ bsub -q gpu-huge \
 	-R'span[hosts=1]' -R'rusage[mem=12000]' -M12000 \
 	whisperx --model large-v2 --align_model WAV2VEC2_ASR_LARGE_LV60K_960H \
 	--diarize --language en \
-	./transcribe_whisperX.sh "input_audio/$input_track"
+	--hf_token "$hf_token" \
+	 "input_audio/$input_track"
 
-input_extensionless="$input_track:r"
-cat "$input_extensionless".ass | grep "\{\\\r\}$" | sed "s/.*\[/\[/g" | sed 's/.\\r\}//g' | sed 's/\|{\\1c&HFF00&\\u1\}//g' > "$input_extensionless".transcript.md
+# generate from json transcript, a markdown format with speaker and text
+python3 parse_json_to_markdown.py
 
 python3 ./upload_notion.py "$creation_time"
 rm input_audio/"$input_extensionless".{wav,m4a}
-rm "$input_extensionless".{srt,ass,tsv,txt,vtt,word.srt}
+rm "$input_extensionless".{srt,tsv,txt,vtt,word.srt,json}
